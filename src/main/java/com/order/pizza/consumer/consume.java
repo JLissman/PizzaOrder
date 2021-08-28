b8db5e005868f32c2b9d21e0f425ea26cea65742
JonathanLissman <facehump90@gmail.com>
2021-08-28 19:16:47 +0200

    cleaning - removing datasource from old proj. & unused imports

--- src/main/java/com/order/pizza/consumer/consume.java
+++ src/main/java/com/order/pizza/consumer/consume.java
@@ -1,18 +1,11 @@
 package com.order.pizza.consumer;
 
-import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.concurrent.TimeoutException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class consume {
--- src/main/java/com/order/pizza/controllers/Order.java
+++ src/main/java/com/order/pizza/controllers/Order.java
@@ -1,11 +1,5 @@
 package com.order.pizza.controllers;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class Order {
     String pizzaId;
     String text;
